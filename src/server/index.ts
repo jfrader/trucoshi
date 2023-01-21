de82ad1ce88dfc3dda4b76ca8482ca6778bd8c65
J Francisco Rader <franciscorader@gmail.com>
2023-01-20 21:45:32 -0300

    server socket

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,22 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+const PORT = 4001
+
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // options
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
 });
 
 io.on("connection", (socket) => {
-  // ...
+    socket.on('ping', (msg) => {
+        io.emit('pong', msg);
+    });
 });
 
-httpServer.listen(3000);
+httpServer.listen(PORT);
+
+console.log('Listening on port', PORT)
