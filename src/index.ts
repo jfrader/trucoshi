28d4b740b750db7d1b40c632aa73e1c8e8ecb1c4
J Francisco Rader <franciscorader@gmail.com>
2023-02-06 14:44:27 -0300

    new release

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./lib"
+export * from "./server"
 export * from "./types"
---
+++ src/trucoshi.ts
@@ -0,0 +1,17 @@
+import { SocketServer, Trucoshi } from "./server/classes"
+import { trucoshiEvents } from "./server/middlewares/trucoshiEvents"
+
+const PORT = process.env.NODE_PORT || 4001
+const ORIGIN = process.env.NODE_ORIGIN || "http://localhost:3000"
+
+const server = SocketServer(Trucoshi(), Number(PORT), [ORIGIN])
+
+server.listen((io) => {
+  console.log("Listening on", PORT, " from origin at", ORIGIN)
+
+  io.use(trucoshiEvents(server))
+
+  io.on("connection", (socket) => {
+    console.log("New socket", socket.id)
+  })
+})
