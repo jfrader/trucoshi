98c459e5eb8489b7e0739fedd022d197473e021b
J Francisco Rader <franciscorader@gmail.com>
2024-01-20 20:36:12 -0300

    use env for auth

--- src/accounts/client.ts
+++ src/accounts/client.ts
@@ -2,13 +2,14 @@ import { Api, User } from "lightning-accounts"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { getPublicKey } from "../utils/config/lightningAccounts"
 
+const token = `${process.env.NODE_LIGHTNING_ACCOUNTS_EMAIL}:${process.env.NODE_LIGHTNING_ACCOUNTS_PASSWORD}`
+
 const api = new Api({
   baseURL: process.env.NODE_LIGHTNING_ACCOUNTS_URL,
   withCredentials: true,
   secure: process.env.NODE_ENV === "production",
   headers: {
-    Cookie:
-      "Lightning-Application-Token=admin@trucoshi.com:trucoshi123aaklsjdlaksdjlkas2ll2j2mmmcjkj1n2n3nn123;",
+    Cookie: `Lightning-Application-Token=${token};`,
   },
 })
 
