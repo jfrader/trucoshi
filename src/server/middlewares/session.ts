a1de7dad1b02b5a4b913bfe4a657abf54fa3ecc7
J Francisco Rader <franciscorader@gmail.com>
2024-02-02 09:25:08 -0300

    small log fix

--- src/server/middlewares/session.ts
+++ src/server/middlewares/session.ts
@@ -6,11 +6,9 @@ import { EClientEvent, EServerEvent } from "../../types"
 import { validateJwt } from "../../accounts/client"
 import { Event } from "socket.io"
 
-const log = logger.child({ middleware: "session" })
-
 export const session = (server: ITrucoshi) => {
   server.io.on("connection", (socket) => {
-    log.debug("New socket connection %s", socket.id)
+    logger.debug("New socket connection %s", socket.id)
     if (socket.data.user) {
       socket.join(socket.data.user.session)
       server.emitSocketSession(socket)
@@ -19,7 +17,7 @@ export const session = (server: ITrucoshi) => {
 
   return (socket: TrucoshiSocket, next: (err?: ExtendedError) => void) => {
     socket.on("disconnect", async (reason) => {
-      log.info("Socket disconnected, reason?: %s", reason)
+      logger.info("Socket disconnected, reason?: %s", reason)
       if (socket.data.user) {
         const matchingSockets = await server.io.in(socket.data.user?.session).fetchSockets()
         const isDisconnected = matchingSockets.length === 0
@@ -27,7 +25,7 @@ export const session = (server: ITrucoshi) => {
           const userSession = server.sessions.get(socket.data.user.session)
           if (userSession) {
             userSession.disconnect()
-            server.cleanupUserTables(userSession).catch(log.error)
+            server.cleanupUserTables(userSession).catch(logger.error)
           }
         }
       }
@@ -36,7 +34,7 @@ export const session = (server: ITrucoshi) => {
     socket.use(validateSession(socket))
 
     socket.on("error", (err) => {
-      log.error(err, "Socket packet error")
+      logger.error(err, "Socket packet error")
       socket.disconnect()
     })
 
@@ -57,7 +55,7 @@ export const session = (server: ITrucoshi) => {
           socket.data.matches = new TMap()
         }
 
-        log.info("Socket %s connected to guest session %s", socket.id, userSession.session)
+        logger.info("Socket %s connected to guest session %s", socket.id, userSession.session)
 
         return next()
       }
@@ -67,7 +65,7 @@ export const session = (server: ITrucoshi) => {
     socket.data.user = userSession.getUserData()
     userSession.connect()
 
-    log.info("Socket %s connected to new guest session %s", socket.id, userSession.session)
+    logger.info("Socket %s connected to new guest session %s", socket.id, userSession.session)
 
     next()
   }
@@ -80,11 +78,11 @@ const validateSession: (
   retry?: number
 ) => (event: Event, next: (err?: Error | undefined) => void) => void = (socket, retry = 0) => {
   return (event, next) => {
-    log.debug("Received event %s from socket %s", event[0], socket.id)
+    logger.debug("Received event %s from socket %s", event[0], socket.id)
     if (NON_VALIDATED_EVENTS.includes(event[0])) {
       return next()
     }
-    log.trace({ ...socket.data.user, event: event[0] }, "validating session")
+    logger.trace({ ...socket.data.user, event: event[0] }, "validating session")
     if (socket.data.user?.account?.id) {
       try {
         if (!socket.data?.identity) {
@@ -94,7 +92,7 @@ const validateSession: (
         return next()
       } catch (e) {
         if (retry < 3) {
-          log.trace({ ...socket.data.user, event: event[0] }, "refreshing identity")
+          logger.trace({ ...socket.data.user, event: event[0] }, "refreshing identity")
           return socket.emit(
             EServerEvent.REFRESH_IDENTITY,
             socket.data.user.account.id,
