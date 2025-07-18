f9a2a60f9b3fdb4e87baa6854d46d0246f2df815
fran <jfrader@pm.me>
2025-07-18 06:11:46 -0300

    fix log

--- src/truco/Hand.ts
+++ src/truco/Hand.ts
@@ -379,7 +379,7 @@ export function Hand(match: IMatch, idx: number) {
         hand.setTurn(hand.turn + 1)
       }
 
-      log.trace({ round: hand.currentRound }, "Calling round next turn")
+      log.trace({ round: hand.currentRound?.cards }, "Calling round next turn")
 
       hand.currentRound?.nextTurn()
     },
