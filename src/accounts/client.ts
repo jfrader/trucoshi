e7a195bcd5e14a49f0e50fece2c15f2e74192dbc
fran <jfrader@pm.me>
2025-08-02 04:13:23 -0300

    add headers

--- src/accounts/client.ts
+++ src/accounts/client.ts
@@ -3,6 +3,7 @@ import jwt, { JwtPayload } from "jsonwebtoken"
 import { getPublicKey } from "../utils/config/lightningAccounts"
 import { SocketError } from "../server"
 import { memoizeMinute } from "../lib/utils"
+import { networkInterfaces } from "os"
 
 const token = `${process.env.APP_LIGHTNING_ACCOUNTS_EMAIL}:${process.env.APP_LIGHTNING_ACCOUNTS_PASSWORD}`
 
@@ -10,12 +11,32 @@ function getCookieName(name: string) {
   return (process.env.APP_LIGHTNING_ACCOUNTS_COOKIE_PREFIX || "") + name
 }
 
+// Get container’s IP dynamically
+const getContainerIp = (): string => {
+  const interfaces = networkInterfaces()
+  const dockerInterface = interfaces["eth0"] || interfaces["en0"] || interfaces["eth1"]
+  if (dockerInterface) {
+    const ipv4 = dockerInterface.find((iface) => iface.family === "IPv4")
+    if (ipv4?.address) {
+      return ipv4.address
+    }
+  }
+  console.warn("FALLING BACK TO 10.29.0.1...")
+  return process.env.DOCKER_CLIENT_IP || "10.29.0.1"
+}
+
+const clientIp = getContainerIp()
+
+console.info("Using " + clientIp + " as X-Real-IP")
+
 const api = new Api({
   baseURL: process.env.APP_LIGHTNING_ACCOUNTS_URL,
   withCredentials: true,
   secure: process.env.NODE_ENV === "production",
   headers: {
     Cookie: `Lightning-Application-Token=${token};`,
+    "X-Forwarded-For": clientIp,
+    "X-Real-IP": clientIp,
   },
 })
 
